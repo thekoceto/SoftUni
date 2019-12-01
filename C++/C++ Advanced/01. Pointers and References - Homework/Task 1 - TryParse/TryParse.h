e956387eba5b4ca43d909a642dba87a6eaeaa3a7
Kostadin Kostadinov <thekoceto@users.noreply.github.com>
2019-12-01 21:36:18 +0200

    Update TryParse.h

--- C++/C++ Advanced/01. Pointers and References - Homework/Task 1 - TryParse/TryParse.h
+++ C++/C++ Advanced/01. Pointers and References - Homework/Task 1 - TryParse/TryParse.h
@@ -1,13 +1,12 @@
 #include <string>
 
 bool tryParse(const std::string &anyText, int &a) {
-
+	
 	for (char ch : anyText)
-	{
 		if (isalpha(ch))
 			return false;
-	}
+	
 	a = std::stoi(anyText);
-
+	
 	return true;
 }
