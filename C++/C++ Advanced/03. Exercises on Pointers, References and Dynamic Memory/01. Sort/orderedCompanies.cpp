77fd4eb349daa202118bd5233709a2ef2a54c935
Kostadin Kostadinov <thekoceto@users.noreply.github.com>
2019-12-15 22:25:56 +0200

    Add files via upload

---
+++ C++/C++ Advanced/03. Exercises on Pointers, References and Dynamic Memory/01. Sort/orderedCompanies.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+
+#include "Company.h"
+
+bool byName(const Company& company1, const Company& company2) {
+	return company1.getName() < company2.getName();
+}
+
+bool byId(const Company& company1, const Company& company2) {
+	return company1.getId() < company2.getId();
+}
+
+void orderCompanies(std::vector <Company> &companies, bool(*funcp)(const Company&, const Company&)) {
+	std::sort(companies.begin(), companies.end(), funcp);
+}
+
+int main() {
+	std::cin.sync_with_stdio(false);
+	std::cout.sync_with_stdio(false);
+
+	std::vector <Company> companies;
+
+	std::ostringstream input;
+	std::string line;
+
+	while (std::getline(std::cin, line) && line != "end") {
+		std::string name;
+		int id = 0;
+		std::stringstream split(line);
+		split >> name >> id;
+		companies.emplace_back(id, name);
+	}
+
+	std::string uniquenessCriteria;
+	std::cin >> uniquenessCriteria;
+	
+	if (uniquenessCriteria == "id")
+		orderCompanies(companies, byId);
+	else if (uniquenessCriteria == "name")
+		orderCompanies(companies, byName);
+
+	for (Company company : companies)
+		std::cout << company.toString() << std::endl;
+
+	return 0;
+}
