bd39dfb3a5f9ec3d10190ebe6284b78a95c55bfa
Kostadin Kostadinov <thekoceto@users.noreply.github.com>
2019-12-21 21:32:11 +0200

    Add files via upload

---
+++ C++/C++ Advanced/04. Advanced C++ Class Members - Homework/Task 2 - SumOfVectors_Description and Code/SumOfVectors.h
@@ -0,0 +1,19 @@
+#ifndef SUMOFVECTORS_H
+#define SUMOFVECTORS_H
+
+#include <string>
+#include <vector>
+
+std::vector<std::string> operator+(const std::vector<std::string> &left, const std::vector<std::string> &right) {
+	
+	size_t SIZE = left.size() > right.size() ? left.size() : right.size();
+	std::vector<std::string> vecResult(SIZE);
+
+	for (size_t i = 0; i < SIZE; i++)
+		vecResult[i] = left[i] + ' ' + right[i];
+
+	return vecResult;
+}
+
+#endif // !SUMOFVECTORS_H
+
